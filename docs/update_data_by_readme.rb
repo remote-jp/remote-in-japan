b5fdd2005b21dbc1dea5d2109bf72aef50861872
Yohei Yasukawa <yohei@yasslab.jp>
2020-02-21 18:42:00 +0900

    Fetch company name and link from README.en.md

--- docs/update_data_by_readme.rb
+++ docs/update_data_by_readme.rb
@@ -1,17 +1,22 @@
 #!/usr/bin/env ruby
 
 require 'kramdown'
+require 'pry'
 
 text = IO.readlines('../README.en.md')[8..-20]
 text.each do |line|
   next unless line.include? '|'
   company = {}
   l = line.split '|'
-  company[:name] = l[1]
+  name_and_link = Kramdown::Document.new(l[1]).root.children[0].children[0]
+
+  company[:name] = name_and_link.children[0].value.strip
+  company[:link] = name_and_link.attr['href']
+  company[:id]   = company[:name].gsub(' ', '_').delete(".,").downcase
   company[:desc] = l[2].strip
   company[:full] = l[3].include?('ok') ? true : false
 
-  puts company
+  puts company[:id]
 end
 
 
