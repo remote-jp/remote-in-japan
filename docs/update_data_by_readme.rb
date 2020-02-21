84594c78062f80b93b4222a3e724c802e98d2b8d
Yohei Yasukawa <yohei@yasslab.jp>
2020-02-21 19:20:31 +0900

    Generate text to write out from README.en.md

--- docs/update_data_by_readme.rb
+++ docs/update_data_by_readme.rb
@@ -8,15 +8,27 @@ readme_ja = IO.readlines('../README.md')[10..-17]
 
 readme_en.each_with_index do |line, index|
   next unless line.include? '|'
-  company = {}
+
   l = line.split '|'
   name_and_link = Kramdown::Document.new(l[1]).root.children[0].children[0]
+  name = name_and_link.children[0].value.strip
+  link = name_and_link.attr['href']
+  id   = name.gsub(' ', '_').delete(".,").downcase
 
-  company[:name] = name_and_link.children[0].value.strip
-  company[:link] = name_and_link.attr['href']
-  company[:id]   = company[:name].gsub(' ', '_').delete(".,").downcase
-  company[:desc] = Kramdown::Document.new(l[2].strip).to_html
-  company[:full] = l[3].include?('ok') ? true : false
+  company =  "---\n"
+  company << "layout: post\n"
+  company << "lang: en\n"
+  company << "permalink: /en/#{id}\n"
+  company << "lang-reference: #{id}\n"
+  company << "title: #{name}\n"
+  company << "description: #{name}\n"
+  company << "date: 2019-01-01 00:00:00 +0900\n" # Not being used
+  company << "by: John Doe\n" # Not being used
+  company << "job_description: #{Kramdown::Document.new(l[2].strip).to_html.strip}\n"
+  company << "is_full_remote: #{l[3].include?('ok') ? true : false}\n"
+  company << "image: ''\n" # Not being used
+  company << "---\n"
 
-  puts company[:desc]
+  puts company
+  puts
 end
