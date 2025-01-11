568e1ec7e30c0b7de50a559057593d28586bd878
yasulab <yasulab@users.noreply.github.com>
2025-01-11 11:53:14 +0000

    deploy: 022f9ba9a76e2c644114fc35dd6f5323bb059b5e

---
+++ DEVELOPMENT.md
@@ -0,0 +1,33 @@
+# remotework.jp
+
+Simple and responsive website for remote work in Japan in Japanese and English.
+
+# Install & Run
+
+1. Git clone repo
+2. ```
+   cd docs
+   bundle install
+   bundle exec jekyll serve
+   ```
+
+## Languages
+
+- Japanese
+- English
+
+## Categories
+
+1. Fully remote jobs: `full_remote`
+2. Japanese required remote jobs: `ja_required`
+
+## Contribute to translation
+
+Adding missing Japanese translation is much appreciated.
+
+- In file `/_data/translations.yml`
+- Add to the translation key the missing translation in `ja`
+
+## License
+
+The theme is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
---
+++ upsert_data_by_readme.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+require 'kramdown'
+require 'sanitize'
+
+lang   = ARGV[0] || 'en'
+readme = if lang == 'en'
+           IO.readlines('../README.en.md')
+         elsif lang == 'ja'
+           IO.readlines('../README.md')
+         else
+           puts "Need to pass [en|ja] to exec this task:"
+           puts "Ex. $ bundle exec rake upsert_data_by_readme:en"
+           puts "Ex. $ bundle exec rake upsert_data_by_readme:ja"
+           puts "Ex. $ bundle exec rake upsert_data_by_readme"
+           puts "    # This generate both data in English and Japanese"
+           exit
+         end
+
+# Remove existing files, parse README, and re-generate them
+Dir.glob("./#{lang}/_posts/*.md").each { |filename| File.delete(filename) }
+
+start_parsing_flag = false
+readme.each_with_index do |line, index|
+  # Code for operating start-of and end-of parsing table of lines in README.
+  if start_parsing_flag == false
+    start_parsing_flag = true if line.start_with? '| ---'
+    next
+  end
+  break if line.start_with? '##' # Stop parsing if reached to next heading.
+
+  # Code for generating Markdown files to publish from remotework.jp
+  next unless line.include? '|'
+  cells = line.gsub('\|', '&#124;').split '|'
+
+  name_and_link = Kramdown::Document.new(cells[1]).root.children[0].children[0]
+  name  = name_and_link.children[0].value.strip
+  link  = name_and_link.attr['href']
+  id    = name.gsub(' ', '_')
+    .gsub('＆', 'and')
+    .gsub('&',  'and')
+    .gsub('（', '(')
+    .gsub('）', ')')
+    .delete(".,").downcase
+
+  full_description = Kramdown::Document.new(cells[2].strip).to_html.strip
+  is_full_remote   = cells[3].include?('ok') ? 'full_remote' : ''
+
+  # Generate a corresponding file by parsed-README data
+  company = <<~COMPANY_PAGE
+    ---
+    layout: post
+    lang: #{lang}
+    permalink: /#{lang}/#{id}
+    title: #{name}
+    description: '#{Sanitize.clean(full_description)}'
+    description_full: '#{full_description}'
+    categories: #{is_full_remote}
+    link: #{link}
+    ---
+  COMPANY_PAGE
+
+  #company << "date: 2019-01-01 00:00:00 +0900\n" # Not being used
+  #company << "by: John Doe\n"                    # Not being used
+  #company << "image: ''\n"                       # Not being used
+
+  IO.write("./#{lang}/_posts/2020-02-22-#{id}.md", company)
+  puts "Upsert: ./#{lang}/_posts/2020-02-22-#{id}.md"
+end
+puts ''
